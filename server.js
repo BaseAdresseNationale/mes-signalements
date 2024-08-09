df5fc6f5acec414dc2dd669c97532697795f429f
Guillaume Fay <guillaume_fay@hotmail.com>
2024-08-09 16:28:15 +0200

    fix sonarcloud

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var express = require('express');
+const express = require('express');
 
-var app = express();
-var directory = '/' + (process.env.STATIC_DIR || 'build')
+const app = express();
+const directory = '/' + (process.env.STATIC_DIR || 'build')
 console.log(__dirname + directory)
 app.use(express.static(__dirname + directory));
 
-var port = process.env.PORT || 3004;
+const port = process.env.PORT || 3004;
 app.listen(port, function () {
   console.log('Listening on', port);
 });
