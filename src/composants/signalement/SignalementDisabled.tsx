8a98f1469dc48a60f7156bca5f01f35cba2dcc68
Guillaume Fay <guillaume_fay@hotmail.com>
2025-05-27 15:47:37 +0200

    feat: add commune status

--- src/composants/signalement/SignalementDisabled.tsx
+++ src/composants/signalement/SignalementDisabled.tsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import Alert from '@codegouvfr/react-dsfr/Alert'
 
-export default function SignalementDisabled() {
+type SignalementDisabledProps = {
+  message?: string
+}
+
+export default function SignalementDisabled({ message }: SignalementDisabledProps) {
   return (
     <Alert
       severity='info'
       title='Les signalements sont désactivés pour cette commune'
       description={
-        <>
-          <p style={{ marginBottom: '1rem', marginTop: '0.5rem' }}>
-            Nous vous recommandons de contacter directement la mairie.
-          </p>
-          <p>
-            <a
-              href='https://adresse-data-gouv-fr.gitbook.io/doc-bal/deposer-un-signalement/mes-signalements'
-              target='_blank'
-              rel='noreferrer'
-            >
-              En savoir plus
-            </a>
-          </p>
-        </>
+        message ? (
+          <div style={{ marginTop: '0.5rem' }} dangerouslySetInnerHTML={{ __html: message }} />
+        ) : (
+          <>
+            <p style={{ marginBottom: '1rem', marginTop: '0.5rem' }}>
+              Nous vous recommandons de contacter directement la mairie.
+            </p>
+            <p>
+              <a
+                href='https://adresse-data-gouv-fr.gitbook.io/doc-bal/deposer-un-signalement/mes-signalements'
+                target='_blank'
+                rel='noreferrer'
+              >
+                En savoir plus
+              </a>
+            </p>
+          </>
+        )
       }
     />
   )
---
+++ src/hooks/useCommuneStatus.tsx
@@ -0,0 +1,43 @@
+import { useContext, useEffect, useState } from 'react'
+import { CommuneStatusDTO, SettingsService } from '../api/signalement'
+import SourceContext from '../contexts/source.context'
+
+type UseCommuneStatusParams = {
+  codeCommune: string
+}
+
+export function useCommuneStatus({ codeCommune }: UseCommuneStatusParams): {
+  communeStatus: CommuneStatusDTO
+  isCommuneStatusLoading: boolean
+} {
+  const [communeStatus, setCommuneStatus] = useState<CommuneStatusDTO>({
+    disabled: true,
+  })
+  const [isCommuneStatusLoading, setIsCommuneStatusLoading] = useState(true)
+  const { source } = useContext(SourceContext)
+  const sourceId = source?.id || process.env.REACT_APP_API_SIGNALEMENT_SOURCE_ID
+
+  useEffect(() => {
+    async function fetchData() {
+      if (!codeCommune || !sourceId) {
+        console.warn('Code commune or source ID is missing')
+        return
+      }
+
+      setIsCommuneStatusLoading(true)
+      try {
+        const _communeStatus = await SettingsService.getCommuneStatus(codeCommune, sourceId)
+        setCommuneStatus(_communeStatus)
+      } catch (error) {
+        console.error('Error fetching commune status:', error)
+        setCommuneStatus({ disabled: true })
+      } finally {
+        setIsCommuneStatusLoading(false)
+      }
+    }
+
+    fetchData()
+  }, [codeCommune, sourceId])
+
+  return { communeStatus, isCommuneStatusLoading }
+}
