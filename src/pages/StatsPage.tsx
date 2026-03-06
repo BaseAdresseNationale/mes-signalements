aae126f519969a9d14b667a4f85f2cab5214203f
Guillaume Fay <guillaume_fay@hotmail.com>
2026-03-06 20:48:12 +0100

    feat: add alert stats

--- src/pages/StatsPage.tsx
+++ src/pages/StatsPage.tsx
@@ -4,6 +4,7 @@ import { Sunburst } from '@ant-design/plots'
 import { useStats } from '../hooks/useStats'
 import { CountStat } from '../composants/stats/CountStat'
 import Loader from '../composants/common/Loader'
+import Tabs from '@codegouvfr/react-dsfr/Tabs'
 
 const StyledWrapper = styled.div`
   display: flex;
@@ -24,50 +25,101 @@ const StyledWrapper = styled.div`
 `
 
 export function StatsPage() {
-  const stats = useStats()
+  const { signalementStats, alertStats, isLoading } = useStats()
 
   return (
     <StyledWrapper>
-      <h3>Tableau de bord statistiques</h3>
-      {stats ? (
-        <>
-          <section>
-            <div className='wrapper'>
-              <CountStat label='Total' count={stats.total} />
-              <CountStat label='En attente' count={stats.totalPending} />
-              <CountStat label='Traités' count={stats.totalProcessed} />
-              <CountStat label='Ignorés' count={stats.totalIgnored} />
-              <CountStat label='Expirés' count={stats.totalExpired} />
-            </div>
-          </section>
-
-          <section>
-            <Sunburst
-              data={stats.sourceChartData}
-              animate={{
-                enter: { type: 'waveIn' },
-              }}
-              innerRadius={0.2}
-              title={{
-                title: 'Créations par sources',
-                titleFontSize: 22,
-              }}
-            />
-            <Sunburst
-              data={stats.clientChartData}
-              animate={{
-                enter: { type: 'waveIn' },
-              }}
-              innerRadius={0.2}
-              title={{
-                title: 'Traitements par clients',
-                titleFontSize: 22,
-              }}
-            />
-          </section>
-        </>
-      ) : (
+      <h1>Tableau de bord statistiques</h1>
+      {isLoading ? (
         <Loader />
+      ) : (
+        <Tabs
+          tabs={[
+            {
+              label: 'Signalements',
+              isDefault: true,
+              content: signalementStats ? (
+                <>
+                  <h2>Signalements</h2>
+                  <section>
+                    <div className='wrapper'>
+                      <CountStat label='Total' count={signalementStats.total} />
+                      <CountStat label='En attente' count={signalementStats.totalPending} />
+                      <CountStat label='Traités' count={signalementStats.totalProcessed} />
+                      <CountStat label='Ignorés' count={signalementStats.totalIgnored} />
+                      <CountStat label='Expirés' count={signalementStats.totalExpired} />
+                    </div>
+                  </section>
+
+                  <section>
+                    <Sunburst
+                      data={signalementStats.sourceChartData}
+                      animate={{
+                        enter: { type: 'waveIn' },
+                      }}
+                      innerRadius={0.2}
+                      title={{
+                        title: 'Créations par sources',
+                        titleFontSize: 22,
+                      }}
+                    />
+                    <Sunburst
+                      data={signalementStats.clientChartData}
+                      animate={{
+                        enter: { type: 'waveIn' },
+                      }}
+                      innerRadius={0.2}
+                      title={{
+                        title: 'Traitements par clients',
+                        titleFontSize: 22,
+                      }}
+                    />
+                  </section>
+                </>
+              ) : null,
+            },
+            {
+              label: 'Alertes',
+              content: alertStats ? (
+                <>
+                  <h2>Alertes</h2>
+                  <section>
+                    <div className='wrapper'>
+                      <CountStat label='Total' count={alertStats.total} />
+                      <CountStat label='En attente' count={alertStats.totalPending} />
+                      <CountStat label='Traités' count={alertStats.totalProcessed} />
+                      <CountStat label='Ignorés' count={alertStats.totalIgnored} />
+                    </div>
+                  </section>
+                  <section>
+                    <Sunburst
+                      data={alertStats.sourceChartData}
+                      animate={{
+                        enter: { type: 'waveIn' },
+                      }}
+                      innerRadius={0.2}
+                      title={{
+                        title: 'Créations par sources',
+                        titleFontSize: 22,
+                      }}
+                    />
+                    <Sunburst
+                      data={alertStats.clientChartData}
+                      animate={{
+                        enter: { type: 'waveIn' },
+                      }}
+                      innerRadius={0.2}
+                      title={{
+                        title: 'Traitements par clients',
+                        titleFontSize: 22,
+                      }}
+                    />
+                  </section>
+                </>
+              ) : null,
+            },
+          ]}
+        />
       )}
     </StyledWrapper>
   )
