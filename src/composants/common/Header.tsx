08c2b1ff8a691e23cc87df76964a3d64b9937784
Guillaume Fay <guillaume_fay@hotmail.com>
2026-06-10 15:25:14 +0200

    fix: minimize header on mobile

--- src/composants/common/Header.tsx
+++ src/composants/common/Header.tsx
@@ -1,20 +1,41 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import { Source } from '../../api/signalement'
 import { AuthentificationModal } from '../authentification/AuthentificationModal'
 import { Header as HeaderDSFR } from '@codegouvfr/react-dsfr/Header'
 import SourcePortal from '../authentification/SourceMenu'
+import { MOBILE_BREAKPOINT } from '../../hooks/useWindowSize'
 
 interface HeaderProps {
   customSource?: Source
   toggleShowInfo: () => void
 }
 
+const StyledHeaderDSFR = styled(HeaderDSFR)`
+  @media screen and (max-width: ${MOBILE_BREAKPOINT}px) {
+    .fr-header__service {
+      display: none;
+    }
+  }
+
+  #header-menu-modal-mes-signalements-header {
+    .fr-container::before {
+      position: absolute;
+      content: 'Mes Signalements';
+      display: block;
+      margin-top: 0.2rem;
+      font-size: 1.2rem;
+      font-weight: 700;
+    }
+  }
+`
+
 export function Header({ customSource, toggleShowInfo }: HeaderProps) {
   const [showConnectionModal, setShowConnectionModal] = useState(false)
 
   return (
     <>
-      <HeaderDSFR
+      <StyledHeaderDSFR
         id='mes-signalements-header'
         brandTop={
           <>
